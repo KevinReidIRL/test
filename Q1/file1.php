8bca415a0df60d71ac8893120c9b094eac1705b5
kif20111 <kif20111@gmail.com>
2016-11-11 09:01:45 -0500

    message

---
+++ Q1/file1.php
@@ -0,0 +1,11 @@
+ <html>
+<body>
+
+<form action="file2.php" method="post">
+What is the name of your favourite book <input type="text" name="name"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html> 
---
+++ Q2/file2.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Your favourite book is <?php echo $_POST["name"]; ?><br>
+
+
+</body>
+</html> 
